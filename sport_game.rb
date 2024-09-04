f40f2574d4b3976a1fbdb206f39202697b661ab2
Long Tran <long.thanh.tran@gmail.com>
2024-09-04 12:45:00 +0700

    Accept file input with argument '-f'

--- sport_game.rb
+++ sport_game.rb
@@ -1,22 +1,60 @@
 class SportGame
+  attr_reader :games
 
-  def load
-    File.read('input.txt')
+  def load(filename)
+    @games = File.read(filename)
   end
 
   def parse
-    load.split("\n")
+    @games.split("\n")
   end
 
   def rule(match)
     regex = /([a-zA-Z\s]+)\s(\d+)/
 
-    match.split(",").map {|t| regex.match(t).captures}.to_h
-      .transform_keys{|k| k.strip.downcase.gsub(/\s/, "_").to_sym}
+    match.split(",")
+      .map{ |t| regex.match(t).captures }.to_h
+      .transform_keys{ |k| k.strip }
       .transform_values(&:to_i)
   end
 
-  def tell_result(matches)
+  def result(match)
+    result = game_result(match)
+    case
+    when result == 0
+      match.transform_values! {|v| v = 1}
+    when result < 0
+      match[match.first.first] = 0
+      match[match.to_a.last.first] = 3
+    when result > 0
+      match[match.first.first] = 3
+      match[match.to_a.last.first] = 0
+    end
+    match
+  end
 
+  def game_result(match)
+    match.values.inject(&:-)
+  end
+
+  def declares
+    parse.map { |match| result(rule(match)) }
+      .inject({}) { |source, target|
+        source.merge(target) { |key, value_1, value_2|
+          value_1 + value_2
+        }
+      }
   end
 end
+
+if ARGV.first == '-f'
+  sport_game = SportGame.new
+  games = sport_game.load(ARGV.last)
+  sport_game.declares.sort_by {|game, point|
+    -point
+  }.each.with_index(1) do |game, idx|
+    point = game.last > 1 ? "pts" : "pt"
+    puts "#{idx}. #{game.first}, #{game.last} #{point}"
+  end
+end
+
--- test_sport_game.rb
+++ test_sport_game.rb
@@ -4,40 +4,70 @@ require './sport_game'
 describe SportGame do
   before do
     @sport_game = SportGame.new
+    @sport_game.load('input.txt')
   end
 
   describe "when load the file, we get each match" do
     it "should be able to read input" do
-      _(@sport_game.load).wont_be_nil
+      _(@sport_game.games).wont_be_nil
     end
 
     it "can parses input as an array" do
       _(@sport_game.parse).must_be_kind_of(Array)
     end
 
+    it "should rule out the overall" do
+      matches = [
+        {"Lions": 3,      "Snakes": 3},
+        {"Tarantulas": 1, "FC Awesome": 0},
+        {"Lions": 1,      "FC Awesome": 1},
+        {"Tarantulas": 3, "Snakes": 1},
+        {"Lions": 4,      "Grouches": 0},
+      ]
+      expected = {
+        "Tarantulas" => 6,
+        "Lions"      => 5,
+        "FC Awesome" => 1,
+        "Snakes"     => 1,
+        "Grouches"   => 0
+      }
+
+      _(@sport_game.declares).must_equal(expected)
+    end
+
     it "should rule out the match" do
       match = "Lions 3, Snakes 3"
-      expected = {:lions => 3, :snakes => 3}
+      expected = {"Lions" => 3, "Snakes" => 3}
 
       _(@sport_game.rule(match)).must_equal(expected)
     end
 
     it "should rule out even with long team name from the match" do
-      match = "Tarantulas 1, Fc Awesome 0"
-      expected = {:tarantulas => 1, :fc_awesome => 0}
+      match = "Tarantulas 1, FC Awesome 0"
+      expected = {"Tarantulas" => 1, "FC Awesome" => 0}
 
       _(@sport_game.rule(match)).must_equal(expected)
     end
 
-    it "should tell the match result" do
-      hashed_matches = [
-        {:lions => 3, :snakes => 3},
-        {:tarantulas => 1, :fc_awesome =>0},
-        {:lions => 1, :fc_awesome => 1}
-      ]
-      expected = {:lions => 2, :snakes => 1, :taratulas => 3, :fc_awesome => 1}
+    it "should tell a win match" do
+      hashed_matches = {"Lions" => 3, "Snakes" => 1}
+      expected = {"Lions" => 3, "Snakes" => 0}
 
-      _(@sport_game.tell_result(hashed_matches)).must_equal(expected)
+      _(@sport_game.result(hashed_matches)).must_equal(expected)
+    end
+
+    it "should tell a draw match" do
+      hashed_matches = {"Lions" => 3, "Snakes" => 3}
+      expected = {"Lions" => 1, "Snakes" => 1}
+
+      _(@sport_game.result(hashed_matches)).must_equal(expected)
+    end
+
+    it "should tell a lose match" do
+      hashed_matches = {"Lions" => 1, "Snakes" => 3}
+      expected = {"Lions" => 0, "Snakes" => 3}
+
+      _(@sport_game.result(hashed_matches)).must_equal(expected)
     end
   end
 end
